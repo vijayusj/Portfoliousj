f85d29853ae511b45003b4579aeac99fd196788f
vijayusj <kinneravijay9@gmail.com>
2023-03-16 20:29:20 +0530

    created first slide

---
+++ src/components/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../sass/_skills.scss';
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h1>hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus harum
+        aliquid quia rem, sunt quibusdam eum minus vitae repellendus deserunt,
+        nam alias nobis beatae quam quaerat laborum nemo atque voluptatibus.
+      </p>
+    </div>
+  );
+};
+
+export default Skills;
