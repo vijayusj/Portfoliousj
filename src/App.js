c08f15c7d33dd84d1953c95c940eb5c1201eb6f0
vijayusj <kinneravijay9@gmail.com>
2023-03-17 17:03:42 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Skills from './components/Skills';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div>
+      {/* <Navigation /> */}
+      <Header />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
